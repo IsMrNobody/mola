672ab9c5d5212470b0a45d95c3efa1733f1e5c1e
ismrnobody <millanquintana9@gmail.com>
2024-09-11 10:45:03 -0400

    mola

---
+++ components/ParallaxHome.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-parallax
+    contain
+    height="300"
+    src="https://res.cloudinary.com/dku13l2ep/image/upload/v1725981476/JARTATE/Ciudad/caracas/Mola/banner6_yrvds2.png"
+  >
+    <v-col class="text-center">
+      <v-img width="180" class="mx-auto mt-12" :src="merchant.logo"> </v-img>
+      <h2>{{ merchant.slogan }}</h2>
+      <h3>Ahora puedes pedir tu delivery cuando quieras!</h3>
+    </v-col>
+  </v-parallax>
+</template>
+
+<script>
+export default {
+  computed: {
+    merchant() {
+      return this.$store.state.merchant
+    },
+  },
+}
+</script>
+
+<style scoped></style>
---
+++ nuxt.config.js
@@ -0,0 +1,105 @@
+import colors from 'vuetify/es5/util/colors'
+
+export default {
+  // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
+  ssr: true,
+  target: 'static',
+
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    titleTemplate: '%s - Mola',
+    title: 'Mola',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'Mola', content: 'Experiencia única y moderna' },
+      { name: 'format-detection', content: 'Arroz frito' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+  ],
+
+  loading: { color: 'secondary' },
+
+  manifest: {
+    name: 'Mola',
+    short_name: 'M',
+    background_color: 'secondary',
+    theme_color: 'tercero',
+    display: "fullscreen",
+    icons: [
+      {
+        "src": "/logoS-512x512.png",
+        "type": "image/png",
+        "sizes": "512x512"
+      },
+      {
+        "src": "/logoS-192x192.png",
+        "type": "image/png",
+        "sizes": "192x192"
+      }
+    ],
+  },
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/eslint
+    '@nuxtjs/eslint-module',
+    // https://go.nuxtjs.dev/vuetify
+    '@nuxtjs/vuetify',
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios',
+    '@nuxtjs/pwa',
+  ],
+  env: {
+    SWEET_API_URL: process.env.SWEET_API_URL,
+    USER_ID: process.env.USER_ID
+  },  
+
+  // Axios module configuration: https://go.nuxtjs.dev/config-axios
+  axios: {},
+
+  // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
+  vuetify: {
+    customVariables: ['~/assets/variables.scss'],
+    theme: {
+      dark: true,
+      themes: {
+        dark: {
+          primary: '#060502',
+          secondary: '#CBCAC8',
+          tercero: '#4C5A1F',
+          cuarto: '#ECA919',
+          accent: colors.grey.darken3,
+          info: colors.teal.lighten1,
+          warning: colors.amber.base,
+          error: colors.deepOrange.accent4,
+          success: colors.green.accent3
+        }
+      }
+    }
+  },
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+  }
+}
